0246fcef4b3b7993b530e87210d6bc47d695b9ad
aditya2499 <adbansal99@gmail.com>
2024-05-13 18:18:48 +0530

    remove dist folder from tracked

--- dist/src/types/contract.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class ContractAddress {
-    private address;
-    private constructor();
-    static isContractAddressValid(address: string): boolean;
-    static fromString(value: string): ContractAddress;
-    asString(): string;
-}
--- dist/src/types/transaction_hash.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class TransactionHash {
-    private transactionHash;
-    private constructor();
-    static isTransactionalHashValid(hash: string): boolean;
-    static fromString(value: string): TransactionHash;
-    asString(): string;
-}
