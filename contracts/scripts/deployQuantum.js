90b8539e5a6e78f284e3bb426129c3817c91e034
utkarsh-21st <utkarshdhing@gmail.com>
2024-09-18 19:18:54 +0530

    deploy Quantum contract

--- contracts/scripts/deployQuantum.js
+++ contracts/scripts/deployQuantum.js
@@ -9,9 +9,9 @@ async function deployQuantum(verifierAddress, initRoot, aggVerifierId) {
 }
 
 async function main() {
-  const verifierAddress = "0x747eA4AEC7Cea38872b47C1eADf67497E52a76e2"
+  const verifierAddress = "0xc01707bD835f6108785DF081C943C7C108FC3D89"
   const initRoot = "0xbd7c1aa6ff0b352c711ef0b55c5b02981b91e340ac257ad4cc27e9bc072a447d"
-  const aggVerifierId = "0x"
+  const aggVerifierId = "0x1a786d63e2ca32d6ab700876d38491780c9c0bcaaa326bb06885f93fe05c2ccf"
   await deployQuantum(verifierAddress, initRoot, aggVerifierId)
 
   // const quantum = await hre.ethers.getContractAt('lib/Quantum.sol:Quantum', "0xd6aD4d6D83803f56032170a9d2ce71b030A1f4BC");
